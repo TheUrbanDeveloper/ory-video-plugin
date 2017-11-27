50581f4437da1b12a0e11db33850369fbe3d06b5
Adam Malowiecki <adam.malowiecki@gmail.com>
2017-11-27 12:57:23 +1000

    Removed form as it isn't used anymore

--- src/Component/index.js
+++ src/Component/index.js
@@ -2,7 +2,6 @@
 import React from 'react'
 import type { ContentPluginProps } from 'ory-editor-core/lib/service/plugin/classes'
 import Display from './Display'
-import Form from './Form'
 import { MuiThemeProvider, createMuiTheme} from 'material-ui/styles';
 
 export type PropTypes = ContentPluginProps<{ src: string, caption: string }>
